0560661c253bb49dfbc3770b5d481c718a479c10
SpiritSeeker <sanjeet029@gmail.com>
2020-01-10 19:36:09 +0530

    Updated load pixel data

--- Experiment1/Experiment1_Linux.cpp
+++ Experiment1/Experiment1_Linux.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
@@ -69,22 +70,36 @@ class BitMap
 			bm_head.colors_used = *(unsigned int*)(bm_header+46);
 			bm_head.colors_imp = *(unsigned int*)(bm_header+50);
 
-			cout << bm_head.file_type << endl;
-			cout << bm_head.file_size << endl;
-			cout << bm_head.reserved1 << endl;
-			cout << bm_head.reserved2 << endl;
-			cout << bm_head.offset_data << endl;
-			cout << bm_head.size << endl;
-			cout << bm_head.width << endl;
-			cout << bm_head.height << endl;
-			cout << bm_head.planes << endl;
-			cout << bm_head.bit_count << endl;
-			cout << bm_head.compression_type << endl;
-			cout << bm_head.image_size << endl;
-			cout << bm_head.x_res << endl;
-			cout << bm_head.y_res << endl;
-			cout << bm_head.colors_used << endl;
-			cout << bm_head.colors_imp << endl;
+			// cout << bm_head.file_type << endl;
+			// cout << bm_head.file_size << endl;
+			// cout << bm_head.reserved1 << endl;
+			// cout << bm_head.reserved2 << endl;
+			// cout << bm_head.offset_data << endl;
+			// cout << bm_head.size << endl;
+			// cout << bm_head.width << endl;
+			// cout << bm_head.height << endl;
+			// cout << bm_head.planes << endl;
+			// cout << bm_head.bit_count << endl;
+			// cout << bm_head.compression_type << endl;
+			// cout << bm_head.image_size << endl;
+			// cout << bm_head.x_res << endl;
+			// cout << bm_head.y_res << endl;
+			// cout << bm_head.colors_used << endl;
+			// cout << bm_head.colors_imp << endl;
+
+			auto dataSize = ((bm_head.width * 3 + 3) & (~3)) * bm_head.height;
+			vector<char> img(dataSize);
+
+			// cout << dataSize << endl;
+
+			bm_stream.read(img.data(), img.size());
+
+			// for (int i = 0; i < dataSize; i+=3)
+			// {
+			// 	cout << (unsigned int)(img[i] & 0xff) << ", ";
+			// 	cout << (unsigned int)(img[i+1] & 0xff) << ", ";
+			// 	cout << (unsigned int)(img[i+2] & 0xff) << endl;
+			// }
 
 			if(bm_head.bit_count == 24){
 				bm_pixelValues = (unsigned char***)malloc(bm_head.height*sizeof(unsigned char**));
@@ -96,12 +111,15 @@ class BitMap
 
 				for (int i = 0; i < bm_head.height; i++){
 					for (int j = 0; j < bm_head.width; j++){
-						bm_stream >> hex >> a;
-						bm_pixelValues[i][j][2] = a;
-						bm_stream >> hex >> a;
-						bm_pixelValues[i][j][1] = a;
-						bm_stream >> hex >> a;
-						bm_pixelValues[i][j][0] = a;	
+						a = img[(bm_head.width*3*i)+j*3] & 0xff;
+						cout << int(a) << ", ";
+						bm_pixelValues[bm_head.height-1-i][j][2] = a;
+						a = img[(bm_head.width*3*i)+j*3+1] & 0xff;
+						cout << int(a) << ", ";
+						bm_pixelValues[bm_head.height-1-i][j][1] = a;
+						a = img[(bm_head.width*3*i)+j*3+2] & 0xff;
+						cout << int(a) << endl;
+						bm_pixelValues[bm_head.height-1-i][j][0] = a;	
 					}
 				}
 			}
@@ -112,7 +130,7 @@ class BitMap
 		int save(const char* filename){
 			ofstream bm_stream(filename, ios::binary);
 
-			cout << sizeof(BMPHeader) << endl;
+			// cout << sizeof(BMPHeader) << endl;
 			// bm_stream.write((char *)(&bm_head), sizeof(BMPHeader));
 
 			// bm_head.planes *= 256;
@@ -139,14 +157,14 @@ class BitMap
 			// bm_head.planes /= 256;
 			// bm_head.bit_count /= 256;
 			
-			// unsigned char a;
+			unsigned char a;
 			for (int i = 0; i < bm_head.height; i++){
 				for (int j = 0; j < bm_head.width; j++){
-					bm_stream << bm_pixelValues[i][j][2];
+					bm_stream << bm_pixelValues[bm_head.height-1-i][j][2];
 					// bm_stream << hex << a;
-					bm_stream << bm_pixelValues[i][j][1];
+					bm_stream << bm_pixelValues[bm_head.height-1-i][j][1];
 					// bm_stream << hex << a;
-					bm_stream << bm_pixelValues[i][j][0];	
+					bm_stream << bm_pixelValues[bm_head.height-1-i][j][0];	
 					// bm_stream << hex << a;
 				}
 			}
@@ -158,24 +176,33 @@ class BitMap
 		
 };
 
-BitMap flip_bitmap(BitMap inp){
-	BitMap out;
-	out.bm_head = inp.bm_head;
-	int temp_h = out.bm_head.height;
-	out.bm_head.height = out.bm_head.width;
-	out.bm_head.width = temp_h;
+// BitMap flip_bitmap(BitMap inp){
+// 	BitMap out;
+// 	out.bm_head = inp.bm_head;
+// 	int temp_h = out.bm_head.height;
+// 	out.bm_head.height = out.bm_head.width;
+// 	out.bm_head.width = temp_h;
 
-	for (int i = 0; i < out.bm_head.height; i++)
-		for (int j = 0; j < out.bm_head.width; j++)
-			out.bm_pixelValues[i][j] = inp.bm_pixelValues[out.bm_head.width - j][out.bm_head.height - i];
+// 	for (int i = 0; i < out.bm_head.height; i++)
+// 		for (int j = 0; j < out.bm_head.width; j++)
+// 			out.bm_pixelValues[i][j] = inp.bm_pixelValues[out.bm_head.width - j][out.bm_head.height - i];
 
-	return out;
-}
+// 	return out;
+// }
 
 int main(int argc, char** argv)
 {
 	BitMap lena;
 	lena.read("lena_colored_256.bmp");
+	// for (int i = 0; i < 128; i++)
+	// 	for (int j = 0; j < 128; j++){
+	// 		lena.bm_pixelValues[i][j][2] = (unsigned char)255;
+	// 		lena.bm_pixelValues[i][j][1] = (unsigned char)0;
+	// 		lena.bm_pixelValues[i][j][0] = (unsigned char)0;
+	// 	}
+	// cout << int(lena.bm_pixelValues[0][0][0]) << endl;
+	// cout << int(lena.bm_pixelValues[0][0][1]) << endl;
+	// cout << int(lena.bm_pixelValues[0][0][2]) << endl;
 	lena.save("lena_ex.bmp");
 	return 0;
 }
